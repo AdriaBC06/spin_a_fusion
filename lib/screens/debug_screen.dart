34dc6b455180e1bd277558a4417ecb225a06476d
Adrià Bonnin Catalán <adriabc06@gmail.com>
2026-01-22 08:54:37 +0100

    generar fusions a debug

--- lib/screens/debug_screen.dart
+++ lib/screens/debug_screen.dart
@@ -19,63 +19,108 @@ class _DebugScreenState extends State<DebugScreen> {
     final pokedex = context.watch<PokedexProvider>();
 
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Pokémon cargados: ${pokedex.pokemonList.length}',
-            style: const TextStyle(fontSize: 18),
-          ),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Pokémon cargados: ${pokedex.pokemonList.length}',
+              style: const TextStyle(fontSize: 18),
+            ),
 
-          const SizedBox(height: 20),
+            const SizedBox(height: 20),
 
-          ElevatedButton(
-            onPressed: pokedex.isLoaded
-                ? () {
-                    setState(() {
-                      pokemon1 = pokedex.getRandomPokemon();
-                      pokemon2 = pokedex.getRandomPokemon();
-                    });
-                  }
-                : null,
-            child: const Text('Generar 2 Pokémon'),
-          ),
+            ElevatedButton(
+              onPressed: pokedex.isLoaded
+                  ? () {
+                      setState(() {
+                        pokemon1 = pokedex.getRandomPokemon();
+                        pokemon2 = pokedex.getRandomPokemon();
+                      });
+                    }
+                  : null,
+              child: const Text('Generar 2 Pokémon'),
+            ),
 
-          const SizedBox(height: 30),
+            const SizedBox(height: 30),
 
-          if (pokemon1 != null) _PokemonInfo(pokemon: pokemon1!),
-          if (pokemon2 != null) _PokemonInfo(pokemon: pokemon2!),
-        ],
+            if (pokemon1 != null && pokemon2 != null)
+              _FusionResult(
+                pokemon1: pokemon1!,
+                pokemon2: pokemon2!,
+              ),
+          ],
+        ),
       ),
     );
   }
 }
 
 /// ------------------------------------------------------
-/// WIDGET PARA MOSTRAR INFO DEL POKÉMON
+/// RESULTADO DE LA FUSIÓN
 /// ------------------------------------------------------
-class _PokemonInfo extends StatelessWidget {
-  final Pokemon pokemon;
+class _FusionResult extends StatelessWidget {
+  final Pokemon pokemon1;
+  final Pokemon pokemon2;
 
-  const _PokemonInfo({required this.pokemon});
+  const _FusionResult({
+    required this.pokemon1,
+    required this.pokemon2,
+  });
+
+  String get _customFusionUrl =>
+      'https://fusioncalc.com/wp-content/themes/twentytwentyone/'
+      'pokemon/custom-fusion-sprites-main/CustomBattlers/'
+      '${pokemon1.id}.${pokemon2.id}.png';
+
+  String get _autoGenFusionUrl =>
+      'https://fusioncalc.com/wp-content/themes/twentytwentyone/'
+      'pokemon/autogen-fusion-sprites-master/Battlers/'
+      '${pokemon1.id}/${pokemon1.id}.${pokemon2.id}.png';
+
+  int get _combinedStats =>
+      pokemon1.totalStats + pokemon2.totalStats;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Column(
-        children: [
-          Text(
-            pokemon.name.toUpperCase(),
-            style: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+    return Column(
+      children: [
+        /// FUSION IMAGE (WITH FALLBACK)
+        Image.network(
+          _customFusionUrl,
+          width: 200,
+          height: 200,
+          fit: BoxFit.contain,
+          errorBuilder: (_, __, ___) {
+            return Image.network(
+              _autoGenFusionUrl,
+              width: 200,
+              height: 200,
+              fit: BoxFit.contain,
+            );
+          },
+        ),
+
+        const SizedBox(height: 16),
+
+        /// NAMES
+        Text(
+          '${pokemon1.name.toUpperCase()} - ${pokemon2.name.toUpperCase()}',
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
-          Text('ID: ${pokemon.id}'),
-          Text('Stats totales: ${pokemon.totalStats}'),
-        ],
-      ),
+          textAlign: TextAlign.center,
+        ),
+
+        const SizedBox(height: 8),
+
+        /// COMBINED STATS
+        Text(
+          'Stats totales: $_combinedStats',
+          style: const TextStyle(fontSize: 16),
+        ),
+      ],
     );
   }
 }
